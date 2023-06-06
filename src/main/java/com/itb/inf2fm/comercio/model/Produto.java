eeb6fb6b7473324e662f7393733a8ea402e8e795
Ana Beatriz Alcântara <ana2404alcantara@gmail.com>
2023-06-06 12:29:43 -0300

    Início controle de versão

---
+++ src/main/java/com/itb/inf2fm/comercio/model/Produto.java
@@ -0,0 +1,19 @@
+package com.itb.inf2fm.comercio.model;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: Acesso liberadp para todas as classes
+	// private: Acesso permitido para apenas os membros da própria classe. Entende-se por membros (atributos e/ou métodos)
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
